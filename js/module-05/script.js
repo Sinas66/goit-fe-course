2dd29def73ae97f50d3ea33e09f81fdf87d040e1
Sinas66 <Pashkomania@gmail.com>
2019-05-04 03:25:32 +0300

    JS Module_05 HW

--- js/module-05/script.js
+++ js/module-05/script.js
@@ -345,80 +345,80 @@
 
 // //========================== ****** task ***** =====================
 
-const scientist = [
-  {
-    first: "Albert",
-    last: "Einstein",
-    year: 1879,
-    passed: 1955
-  },
-  {
-    first: "Isaac",
-    last: "Newton",
-    year: 1643,
-    passed: 1727
-  },
-  {
-    first: "Galileo",
-    last: "Galilei",
-    year: 1564,
-    passed: 1642
-  },
-  {
-    first: "Marie",
-    last: "Curie",
-    year: 1867,
-    passed: 1934
-  },
-  {
-    first: "Johannes",
-    last: "Kepler",
-    year: 1571,
-    passed: 1630
-  },
-  {
-    first: "Nicolaus",
-    last: "Copernicus",
-    year: 1473,
-    passed: 1543
-  },
-  {
-    first: "Max",
-    last: "Planck",
-    year: 1858,
-    passed: 1947
-  },
-  {
-    first: "Katherine",
-    last: "Blodgett",
-    year: 1898,
-    passed: 1979
-  },
-  {
-    first: "Ada",
-    last: "Lovelace",
-    year: 1815,
-    passed: 1852
-  },
-  {
-    first: "Sarah E.",
-    last: "Goode",
-    year: 1855,
-    passed: 1905
-  },
-  {
-    first: "Lise",
-    last: "Meitner",
-    year: 1878,
-    passed: 1968
-  },
-  {
-    first: "Hanna",
-    last: "Hammarström",
-    year: 1829,
-    passed: 1909
-  }
-];
+// const scientist = [
+//   {
+//     first: "Albert",
+//     last: "Einstein",
+//     year: 1879,
+//     passed: 1955
+//   },
+//   {
+//     first: "Isaac",
+//     last: "Newton",
+//     year: 1643,
+//     passed: 1727
+//   },
+//   {
+//     first: "Galileo",
+//     last: "Galilei",
+//     year: 1564,
+//     passed: 1642
+//   },
+//   {
+//     first: "Marie",
+//     last: "Curie",
+//     year: 1867,
+//     passed: 1934
+//   },
+//   {
+//     first: "Johannes",
+//     last: "Kepler",
+//     year: 1571,
+//     passed: 1630
+//   },
+//   {
+//     first: "Nicolaus",
+//     last: "Copernicus",
+//     year: 1473,
+//     passed: 1543
+//   },
+//   {
+//     first: "Max",
+//     last: "Planck",
+//     year: 1858,
+//     passed: 1947
+//   },
+//   {
+//     first: "Katherine",
+//     last: "Blodgett",
+//     year: 1898,
+//     passed: 1979
+//   },
+//   {
+//     first: "Ada",
+//     last: "Lovelace",
+//     year: 1815,
+//     passed: 1852
+//   },
+//   {
+//     first: "Sarah E.",
+//     last: "Goode",
+//     year: 1855,
+//     passed: 1905
+//   },
+//   {
+//     first: "Lise",
+//     last: "Meitner",
+//     year: 1878,
+//     passed: 1968
+//   },
+//   {
+//     first: "Hanna",
+//     last: "Hammarström",
+//     year: 1829,
+//     passed: 1909
+//   }
+// ];
 
 // // 1) отримати масив вчених що народилися в 19 ст
 // const XIX = mass => {
@@ -490,202 +490,255 @@ const scientist = [
 
 // // 7) Знайти вченого який народився найпізніше.
 
-const birthLate = () => {
-  let result = 0;
-  if (scientist[0].year < result || result === 0) {
-    result = scientist.year;
-    console.log(result);
-  }
+// const birthLate = mass => {
+//   let result = 0;
+//   mass.reduce((acc, el, ind) => {
+//     if (mass[ind].year > result) {
+//       result = mass[ind].year;
+//     }
 
-  return result;
-};
+//     // return result;
+//   });
+//   console.log(result);
 
-console.log(scientist.filter(birthLate));
+//   // return result;
+// };
+
+// console.log(birthLate(scientist));
 
 // // 8) Знайти рік народження Albert Einstein
 // // 9) Знайти вчених прізвище яких починається на літеру С
 // // 10) Видалити з масива всіх вчених імя яких починається на A
 
 // //========================== HOMEWORK =====================
+const users = [
+  {
+    id: "701b29c3-b35d-4cf1-a5f6-8b12b29a5081",
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    phone: "+1 (848) 556-2344",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    skills: ["ipsum", "lorem"],
+    gender: "male",
+    age: 37
+  },
+  {
+    id: "7a3cbd18-57a1-4534-8e12-1caad921bda1",
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    phone: "+1 (855) 582-2464",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+    gender: "female",
+    age: 34
+  },
+  {
+    id: "88beb2f3-e4c2-49f3-a0a0-ecf957a95af3",
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    phone: "+1 (814) 593-3825",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+    gender: "male",
+    age: 24
+  },
+  {
+    id: "249b6175-5c30-44c6-b154-f120923736f5",
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    phone: "+1 (909) 547-2687",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    skills: ["adipisicing", "irure", "velit"],
+    gender: "female",
+    age: 21
+  },
+  {
+    id: "334f8cb3-eb04-45e6-abf4-4935dd439b70",
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    phone: "+1 (956) 512-2693",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    skills: ["ex", "culpa", "nostrud"],
+    gender: "male",
+    age: 27
+  },
+  {
+    guid: "150b00fb-dd82-427d-9faf-2879ea87c695",
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    phone: "+1 (876) 411-2433",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    skills: ["non", "amet", "ipsum"],
+    gender: "male",
+    age: 38
+  },
+  {
+    id: "e1bf46ab-7168-491e-925e-f01e21394812",
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    phone: "+1 (979) 504-2554",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    skills: ["lorem", "veniam", "culpa"],
+    gender: "female",
+    age: 39
+  }
+];
+/*
+ * Используя массив (users) объектов пользователей, напишите функции которые с помощью
+ * функциональных методов массивов (никаких for, splice и т.д.) выполняют указанные операции.
+ */
 
-// /*
-//  * Используя массив (users) объектов пользователей, напишите функции которые с помощью
-//  * функциональных методов массивов (никаких for, splice и т.д.) выполняют указанные операции.
-//  */
+/**
+ * Получить массив имен (поле name) всех пользователей
+ */
+const getAllNames = arr => {
+  return arr.reduce((acc, el) => {
+    acc.push(el.name);
+    return acc;
+  }, []);
+};
 
-// /**
-//  * Получить массив имен (поле name) всех пользователей
-//  */
-// const getAllNames = arr => {...};
+console.log(getAllNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
 
-// console.log(getAllNames(users));
-// // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+/**
+ * Получить массив объектов пользователей по цвету глаз (поле eyeColor)
+ */
+const getUsersByEyeColor = (arr, color) => {
+  // return arr.reduce((acc, el) => {
+  //   if (el.eyeColor === color) {
+  //     acc.push(el);
+  //   }
+  //   return acc;
+  // }, []);
 
-// /**
-//  * Получить массив объектов пользователей по цвету глаз (поле eyeColor)
-//  */
-// const getUsersByEyeColor = (arr, color) => {...};
+  return arr.filter(el => (el.eyeColor === color ? el : null));
+};
 
-// console.log(getUsersByEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+console.log(getUsersByEyeColor(users, "blue")); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
 
-// /**
-//  * Получить массив имен пользователей по полу (поле gender)
-//  */
-// const getUsersByGender = (arr, gender) => {...};
+/**
+ * Получить массив имен пользователей по полу (поле gender)
+ */
+const getUsersByGender = (arr, gender) => {
+  return arr.reduce((acc, el) => {
+    el.gender === gender ? acc.push(el.name) : null;
+    return acc;
+  }, []);
+};
 
-// console.log(getUsersByGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+console.log(getUsersByGender(users, "male")); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
 
-// /**
-//  * Получить массив только неактивных пользователей (поле isActive)
-//  */
-// const getInactiveUsers = arr => {...};
+/**
+ * Получить массив только неактивных пользователей (поле isActive)
+ */
+const getInactiveUsers = arr => {
+  return arr.filter(el => (!el.isActive ? el : null));
+};
 
-// console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
+console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
 
-// /**
-//  * Получить пользоваля (не массив) по email (поле email, он уникальный)
-//  */
-// const getUserByEmail = (arr, email) => {...};
+/**
+ * Получить пользоваля (не массив) по email (поле email, он уникальный)
+ */
+const getUserByEmail = (arr, email) => {
+  return arr.find(el => el.email == email);
+};
 
-// console.log(getUserByEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
-// console.log(getUserByEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
+console.log(getUserByEmail(users, "shereeanthony@kog.com")); // {объект пользователя Sheree Anthony}
+console.log(getUserByEmail(users, "elmahead@omatom.com")); // {объект пользователя Elma Head}
 
-// /**
-//  * Получить массив пользователей попадающих в возрастную категорию от min до max лет (поле age)
-//  */
-// const getUsersWithAge = (arr, min, max) => {...};
+/**
+ * Получить массив пользователей попадающих в возрастную категорию от min до max лет (поле age)
+ */
+const getUsersWithAge = (arr, min, max) => {
+  return arr.reduce((acc, el) => {
+    let age = el.age;
+    // if (age > min && age < max) {
+    //   acc.push(el);
+    // }
+    age > min && age < max ? acc.push(el) : null;
+    return acc;
+  }, []);
+};
 
-// console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
 
-// console.log(getUsersWithAge(users, 30, 40));
-// // [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
+console.log(getUsersWithAge(users, 30, 40));
+// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
 
-// /**
-//  * Получить общую сумму баланса (поле balance) всех пользователей
-//  */
-// const getTotalBalance = arr => {...};
+/**
+ * Получить общую сумму баланса (поле balance) всех пользователей
+ */
 
-// console.log(getTotalBalance(users)); // 20916
+const getTotalBalance = arr => {
+  return arr.reduce((acc, el) => acc + el.balance, 0);
+};
 
-// /**
-//  * Массив имен всех пользователей у которых есть друг с указанным именем
-//  */
-// const getUsersByFriend = (arr, name) => {...};
+console.log(getTotalBalance(users)); // 20916
 
-// console.log(getUsersByFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
-// console.log(getUsersByFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
+/**
+ * Массив имен всех пользователей у которых есть друг с указанным именем
+ */
+const getUsersByFriend = (arr, name) => {
+  return arr.reduce((acc, el) => {
+    el.friends.includes(name) ? acc.push(el) : null;
+    return acc;
+  }, []);
 
-// const users = [
-//   {
-//     id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081',
-//     name: 'Moore Hensley',
-//     email: 'moorehensley@indexia.com',
-//     eyeColor: 'blue',
-//     phone: '+1 (848) 556-2344',
-//     friends: ['Sharron Pace'],
-//     isActive: false,
-//     balance: 2811,
-//     skills: ['ipsum', 'lorem'],
-//     gender: 'male',
-//     age: 37,
-//   },
-//   {
-//     id: '7a3cbd18-57a1-4534-8e12-1caad921bda1',
-//     name: 'Sharlene Bush',
-//     email: 'sharlenebush@tubesys.com',
-//     eyeColor: 'blue',
-//     phone: '+1 (855) 582-2464',
-//     friends: ['Briana Decker', 'Sharron Pace'],
-//     isActive: true,
-//     balance: 3821,
-//     skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
-//     gender: 'female',
-//     age: 34,
-//   },
-//   {
-//     id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
-//     name: 'Ross Vazquez',
-//     email: 'rossvazquez@xinware.com',
-//     eyeColor: 'green',
-//     phone: '+1 (814) 593-3825',
-//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
-//     isActive: false,
-//     balance: 3793,
-//     skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
-//     gender: 'male',
-//     age: 24,
-//   },
-//   {
-//     id: '249b6175-5c30-44c6-b154-f120923736f5',
-//     name: 'Elma Head',
-//     email: 'elmahead@omatom.com',
-//     eyeColor: 'green',
-//     phone: '+1 (909) 547-2687',
-//     friends: ['Goldie Gentry', 'Aisha Tran'],
-//     isActive: true,
-//     balance: 2278,
-//     skills: ['adipisicing', 'irure', 'velit'],
-//     gender: 'female',
-//     age: 21,
-//   },
-//   {
-//     id: '334f8cb3-eb04-45e6-abf4-4935dd439b70',
-//     name: 'Carey Barr',
-//     email: 'careybarr@nurali.com',
-//     eyeColor: 'blue',
-//     phone: '+1 (956) 512-2693',
-//     friends: ['Jordan Sampson', 'Eddie Strong'],
-//     isActive: true,
-//     balance: 3951,
-//     skills: ['ex', 'culpa', 'nostrud'],
-//     gender: 'male',
-//     age: 27,
-//   },
-//   {
-//     guid: '150b00fb-dd82-427d-9faf-2879ea87c695',
-//     name: 'Blackburn Dotson',
-//     email: 'blackburndotson@furnigeer.com',
-//     eyeColor: 'brown',
-//     phone: '+1 (876) 411-2433',
-//     friends: ['Jacklyn Lucas', 'Linda Chapman'],
-//     isActive: false,
-//     balance: 1498,
-//     skills: ['non', 'amet', 'ipsum'],
-//     gender: 'male',
-//     age: 38,
-//   },
-//   {
-//     id: 'e1bf46ab-7168-491e-925e-f01e21394812',
-//     name: 'Sheree Anthony',
-//     email: 'shereeanthony@kog.com',
-//     eyeColor: 'brown',
-//     phone: '+1 (979) 504-2554',
-//     friends: ['Goldie Gentry', 'Briana Decker'],
-//     isActive: true,
-//     balance: 2764,
-//     skills: ['lorem', 'veniam', 'culpa'],
-//     gender: 'female',
-//     age: 39,
-//   },
-// ];
+  // return arr.friends.filter(el => el === name);
+};
 
-// /*
-//   ⚠️ ЗАДАНИЕ ПОВЫШЕННОЙ СЛОЖНОСТИ - ВЫПОЛНЯТЬ ПО ЖЕЛАНИЮ
-// */
+console.log(getUsersByFriend(users, "Briana Decker")); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersByFriend(users, "Goldie Gentry")); // [ 'Elma Head', 'Sheree Anthony' ]
 
-// /**
-// * Получить массив всех скиллов всех пользователей (поле skills), при этом не должно быть
-// * повторяющихся скиллов и они должны быть отсортированы в алфавитном порядке
-// */
-// const getAllUniqueSkills = arr => {...};
+/*
+  ⚠️ ЗАДАНИЕ ПОВЫШЕННОЙ СЛОЖНОСТИ - ВЫПОЛНЯТЬ ПО ЖЕЛАНИЮ
+*/
 
-// console.log(getAllSkills(users));
-// // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
+/**
+ * Получить массив всех скиллов всех пользователей (поле skills), при этом не должно быть
+ * повторяющихся скиллов и они должны быть отсортированы в алфавитном порядке
+ */
+const getAllUniqueSkills = arr => {
+  return arr.reduce((acc, el) => {
+    el.skills.forEach(elm => (!acc.includes(elm) ? acc.push(elm) : null));
+    return acc.sort();
+  }, []);
+};
 
-// /**
-// * Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
-// */
-// const getUserNamesSortedByFriendsCount = arr => {...};
+console.log(getAllUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
 
-// console.log(getUserNamesSortedByFriendsCount(users));
-// // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+/**
+ * Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
+ */
+const getUserNamesSortedByFriendsCount = arr => {
+  let mass = arr.slice(0);
+  mass.sort((a, b) => a.friends.length - b.friends.length);
+  return mass.map(el => el.name);
+};
+
+console.log(getUserNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
